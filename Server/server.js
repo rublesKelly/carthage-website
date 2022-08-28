bc84089f050cbf94762c8510ef22b028cb0e69cc
rublesKelly <rkellymkk@gmail.com>
2022-08-28 13:40:47 +0100

    client started

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,16 @@
 //Imports
-const  express = require('express');  
+// const  express = require('express');  
+import express from 'express'
 
 //Setup 
 const  app = express();  
 const  server = require('http').createServer(app);  
-const io = require('socket.io')(server); 
+const  io = require('socket.io')(server); 
 const  port = 3001
 	
 //Initialise server on port 3001
 server.listen(port, function(){
-  console.log(`listening on port 3001`);
+  console.log(`listening on port ${port}`);
 }); 
 
 //redirect / to index.html
@@ -19,6 +20,6 @@ server.listen(port, function(){
 
 //Client connection 
 //For now just console log 
-io.on('connection', function(client) {  
+io.on('connection', (client) => {  
     console.log('Client connected...');
 });
